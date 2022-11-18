93e970c2b2422ed33842affdc676fae76ffb4a22
Nuri <nuri1999sept@gmail.com>
2022-11-18 11:54:26 +0600

    2nd stage

---
+++ if-else/main.go
@@ -0,0 +1,60 @@
+///1st statement
+/*if condition {
+}
+else {
+}*/
+///multiole statement
+/*if condition_1{
+	//code
+} else if condition_2 {
+	//code
+} else if condition_3 {
+	//code
+} else {
+	//code
+}*/
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var a int = 36
+// 	var b int = 20
+
+// 	if a < b {
+// 		fmt.Println("a is less than b")
+// 	} else {
+// 		fmt.Println("a is not less than b")
+// 	}
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var a int = 20
+// 	var b int = 20
+
+// 	if a < b {
+// 		fmt.Println("a is less than b")
+// 	} else if a == b {
+// 		fmt.Println("a is equal to b")
+// 	} else {
+// 		fmt.Println("a is greater to b")
+// 	}
+// }
+
+// if 9%2 == 0 {
+// 	fmt.Printf(even)
+// } else {
+// 	fmt.printf(odd)
+// }
+
+//for web req handlingg this procesas
+// if num:=3; num<10{
+// 	fmt.Println("less than 10")
+// } else{
+// 	fmt.Println("not less than 10")
+// }
