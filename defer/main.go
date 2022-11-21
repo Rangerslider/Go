631f9ac90a887107149d87a077903373e4da5d0c
Nuri <nuri1999sept@gmail.com>
2022-11-21 12:56:06 +0600

    2.01 stage

---
+++ defer/main.go
@@ -0,0 +1,23 @@
+// line by line execution hoi go te kintu defer use korle oita akdom last a hoi
+// last in fast out manner
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("World")
+	defer fmt.Println("One")
+	defer fmt.Println("Two")
+	fmt.Println("Hello")
+	muDefer() // last in thats why it will run fast
+
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Print(i)
+	}
+}
+
+// hello, 43210, two, One, world
+//defer maintain last in first out backside to forntside moving
